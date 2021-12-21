c244ad7eb56575b53e125f51e14783227486dfb9
peter88tom <peter88tom@gmail.com>
2021-12-21 02:14:17 +0200

    Delete a post by index

--- main.py
+++ main.py
@@ -28,6 +28,11 @@ def find_post(id):
       return post
 
 
+# Delete a post
+def find_index_post(id):
+  for i, p in enumerate(my_posts):
+    if p["id"] == id:
+      return i
 
 @app.get("/")
 def root():
@@ -61,6 +66,34 @@ def get_post(id: int):
     # response.status_code = status.HTTP_404_NOT_FOUND
     # return {"message": f"post with {id} was not found"}
   return {"data": post}
+
+
+
+# Delete a post
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+  # delete a post
+  # find the index in the array for request id and pop it out of array
+  index = find_index_post(id)
+  if index == None:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The post with id: {id} does not exists")
+  my_posts.pop(index)
+  return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+
+# Update a post
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+  index = find_index_post(id)
+  if index == None:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The post with id: {id} does not exists")
+  post_dict = post.dict()
+  post_dict["id"] = id
+  print(post_dict)
+  my_posts[index] = post_dict
+  return {"data": post_dict}
+
 
 
 
