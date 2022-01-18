2464d0030c9d83a782dedba509e3f446f59b3c8c
peter88tom <peter88tom@gmail.com>
2022-01-18 03:53:56 +0200

    Retrieve information of a specific user

--- orm_app/main.py
+++ orm_app/main.py
@@ -131,3 +131,15 @@ def create_user(user: schemas.CreateUser, db: Session = Depends(get_db)):
   db.refresh(new_user)
 
   return new_user
+
+
+
+# Get information about user
+@app.get("/user/{id}", response_model= schemas.CreateUserResponse)
+def get_user(id: int, db: Session = Depends(get_db)):
+  user = db.query(models.User).filter(models.User.id == id).first()
+
+  if not user:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"User with id: {id} does not exist")
+  return  user
