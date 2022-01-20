ad3e871d208aa0f520e595bebbad9d7813da765e
peter88tom <peter88tom@gmail.com>
2022-01-20 11:44:17 +0200

    Add owner id to posts table as foreignkey from users table

--- orm_app_with_routers/models.py
+++ orm_app_with_routers/models.py
@@ -13,6 +13,7 @@ class Post(Base):
   published = Column(Boolean, default=True)
   created_at = Column(TIMESTAMP(timezone=True),
                       nullable=False, server_default=text('now()'))
+  owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
 
 
 class User(Base):
