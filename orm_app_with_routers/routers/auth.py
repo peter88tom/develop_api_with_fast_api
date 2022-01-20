0e8beb08010d5ce8b6e2104d8fa1897700cca6f2
peter88tom <peter88tom@gmail.com>
2022-01-20 02:39:47 +0200

    Add schema validation on login end point

--- orm_app_with_routers/routers/auth.py
+++ orm_app_with_routers/routers/auth.py
@@ -36,7 +36,7 @@ This form uses username and not email,
 "password": "some password"
 }
 """
-@router.post("/login")
+@router.post("/login", response_model=schemas.Token)
 def login(user_credentials: OAuth2PasswordRequestForm = Depends() , db: Session = Depends(database.get_db)):
 
   user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
