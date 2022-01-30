069178caba63ead42129205dcb456e89192bfeb8
peter88tom <peter88tom@gmail.com>
2022-01-30 02:00:26 +0200

    Add ability to limit number of returned posts

--- orm_app_with_routers/routers/post.py
+++ orm_app_with_routers/routers/post.py
@@ -14,11 +14,14 @@ router = APIRouter(
 # Get list of posts
 @router.get("/", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db),
-                current_user: int = Depends(oauth2.get_current_user)):
+                current_user: int = Depends(oauth2.get_current_user),
+              limit: int = 10):
   print(current_user.email)
+  print(limit)
   # cursor.execute("""SELECT * FROM posts""")
   # posts= cursor.fetchall()
   posts = db.query(models.Post).all()
+  posts = db.query(models.Post).limit(limit).all()
   # return {"data": posts}
   return posts
 
