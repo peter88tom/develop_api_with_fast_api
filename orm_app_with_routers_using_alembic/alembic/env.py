ea130e27ca380be21c99d1de07ffe888faad3486
peter88tom <peter88tom@gmail.com>
2022-02-15 03:36:16 +0200

    Give alembic access to the database models

--- orm_app_with_routers_using_alembic/alembic/env.py
+++ orm_app_with_routers_using_alembic/alembic/env.py
@@ -5,6 +5,9 @@ from sqlalchemy import pool
 
 from alembic import context
 
+# Give access to database models
+from orm_app_with_routers_using_alembic.database import Base
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -17,7 +20,9 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+# target_metadata = None
+target_metadata = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
